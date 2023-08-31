e50043158f050c783b559930ad2feeaf3b4f2eaf
Stefan Strand <stefanboytchev@hotmail.com>
2023-08-31 13:34:18 +0200

    Completed task 5

---
+++ Oblig1/oppgave5.py
@@ -0,0 +1,12 @@
+p1 = 5
+p2 = 9
+p3 = 2.5
+p4 = 21
+p5 = 0
+
+total = p1 + p2 + p3 + p4 + p5
+numOfCookieEaters = 5
+
+result = total / numOfCookieEaters
+print(str(total) + " / " + " 5 " + " = " + str(int(result)))
+
