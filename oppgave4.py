b9b6f129be914cfa4f1e0ea221e3ff8c61c17820
Stefan Strand <stefanboytchev@hotmail.com>
2023-09-18 01:48:55 +0200

    Added new comments

--- oppgave4.py
+++ oppgave4.py
@@ -8,10 +8,13 @@ books_tolkien = ['Lord of the Rings: Farmer Giles of Ham', 'Lord of the Rings: T
 
 for books in books_tolkien[0:3]:  # Making a variable called "books", which stores books_tolkien index 0:3
     lord_of_the_rings.append(books)  # Using the append function to add the desired books to the empty list.
+    # If extend method is used, (within the for loop), it iterates through every letter in the
+    # name of the books/elements.
 
 print(lord_of_the_rings)  # if print statement is put inside the for loop, it prints as an iteration.
 
-lord_of_the_rings = []  # If list is not reset, then print statement second time doubles the amount of books printed.
+lord_of_the_rings = []  # If list is not reset, then print statement second time around doubles
+# the amount of books printed.
 
 for books in books_tolkien:
     if "Lord of the Rings" in books:
