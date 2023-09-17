239f56b67bd1e940c6d24af310ff1907f7f10818
Stefan Strand <stefanboytchev@hotmail.com>
2023-09-17 22:55:17 +0200

    Revised oblig 1 where i got feedback to use f-strings. this is now in place and understood.

--- Oblig1/oppgave2.py
+++ Oblig1/oppgave2.py
@@ -5,3 +5,11 @@ age = str(35)  # Using the str function to convert the number to a string,
 
 result = "Hei. Jeg heter " + first_name + " " + last_name + " og er " + age + " år gammel."
 print(result)
+
+#  Change after feedback on oblig 1: Use f-strings! See below:
+
+result = f"Hei. Jeg heter {first_name} {last_name} og er {age} år gammel."
+print(result)
+# f-strings: is a formatted string literal that allows you to embed expressions inside string literals.
+# Has to be prefixed with "f" or "F". Introduced in python 3.6. Variables and expressions can be
+# embedded straight into strings using surrounding curly brackets.
--- oppgave1.py
+++ oppgave1.py
@@ -4,7 +4,7 @@ num = int(input("Hva er svaret på det ultimate spørsmålet om livet, universet
 
 if num == 42:
     print("Det stemmer, meningen med livet er 42!")
-elif 30 <= num <= 50 and num != 42:  # This is called a chained comparison.
+elif 30 <= num <= 50 and num != 42:  # This is a chained comparison.
     print("Nærme, men feil.")
 else:
     print("FEIL!")
