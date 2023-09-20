3dfa145f736fa624cb8d5509180008b6d2799560
Stefan Strand <stefanboytchev@hotmail.com>
2023-09-20 05:50:38 +0200

    Making progress slowly task6

--- oppgave6.py
+++ oppgave6.py
@@ -3,9 +3,10 @@ import random
 num_of_players = int(input("Enter number of players:"))
 
 player_score = 0
-for i in range(num_of_players):
-    throws = (random.randrange(0, 60))
-    print(throws)
-    player_score += throws  # score = score + throws. Not using the shorthand.
+for num in range(num_of_players):
+    for darts in range(3):
+        throws = (random.randrange(0, 60))
+        print(throws)
+        player_score += throws  # score = score + throws. Not using the shorthand.
 
 print(f"The total score is {player_score}")
