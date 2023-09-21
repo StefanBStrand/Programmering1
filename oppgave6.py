543b66e8cb3d0eec704cf3fb0c943c4eee4ebb2e
Stefan Strand <stefanboytchev@hotmail.com>
2023-09-21 17:06:27 +0200

    Added functionality to program. Updated prompts and added case insensitive check when removing items from packing list.

--- oppgave6.py
+++ oppgave6.py
@@ -3,27 +3,35 @@
 
 packing_list = []
 packing = True
+first_iteration = True
 question = "What would you like to do?"
 
 print(question)
 print("To add item type Add.\nTo remove item type Remove.\nTo print list type Print.\nTo stop packing type Stop")
 while packing:
-    decision = input("Make your choice: ")
-    if decision == "Add":
-        item_to_add = input("What do you want to add?: ") # Noe skal legges til i packing_list
+    if first_iteration:
+        decision = input("Make your choice: ").lower()
+        first_iteration = False
+    else:
+        decision = input("What would you like to do now?:").lower()
+    if decision == "add":
+        item_to_add = input("What do you want to add?: ").lower()
         packing_list.append(item_to_add)
-    elif decision == "Remove":
-        item_to_remove = input("What would you like to remove?:")
-        packing_list.remove(item_to_remove)
-    elif decision == "Print":
+    elif decision == "remove":
+        item_to_remove = input("What would you like to remove?:").lower()
+        if item_to_remove in packing_list:
+            packing_list.remove(item_to_remove)
+            print(f"{item_to_remove} was removed from packing list")
+        else:
+            print(f"{item_to_remove} not in packing list")
+    elif decision == "print":
         print(packing_list)
-    elif decision == "Stop":
+    elif decision == "stop":
         packing = False
     else:
-        print("invalid choice")
+        print("Invalid choice")
 
-# Need to fix case sensitive stuff. Review user input prompts. 
-
-
-
-# må legge til if checker for å bestemme hva som skal skje basert på bruker input
+# Need to fix case-sensitive stuff. - done
+# Review user input prompts - done
+# WHen user types incorrect command - give list of correct commands
+# Need to check if item is in list before removing. - done
