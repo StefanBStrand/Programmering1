e3711978576c810e85d5e313f1cce981a29f159b
Stefan Strand <stefanboytchev@hotmail.com>
2023-09-22 20:39:59 +0200

    Finished bonus task1, cleaned up code

--- bonusoppg1.py
+++ bonusoppg1.py
@@ -7,34 +7,31 @@ num_of_darts = int(input("Enter number of darts:"))
 num_of_rounds = int(input("Enter number of rounds:"))
 
 player_score = 0
+dart_scores = []
+total_score = []
 
 #  dart_scores = [i for i in range(1, 21)] + [2*i for i in range(1, 21)] + [3*i for i in range(1,21)] + [0, 25, 50]
-dart_smores = []
+#  Ville forenkle måten å bygge listen dart_scores på, leste meg frem til denne metoden over - list comprehension.
+#  Synes det er en enkel og pen måte å bygge listen på. Endte opp med å bruke en for-løkke til å gjøre samme sak.
 
 for i in range(1, 21):
-    dart_smores.append(i)
-    dart_smores.append(2*i)
-    dart_smores.append(3*i)
+    dart_scores.append(i)
+    dart_scores.append(2*i)
+    dart_scores.append(3*i)
 
-dart_smores.append(0)
-dart_smores.append(25)
-dart_smores.append(50)
-
-
-total_score = []
+dart_scores.append(0)
+dart_scores.append(25)
+dart_scores.append(50)
 
 for num in range(num_of_players):
     print(f"**Player {num +1}**")
     for rounds in range(num_of_rounds):
         print(f"Round {rounds +1}")
         for throws in range(num_of_darts):
-            score = random.choice(dart_smores)
+            score = random.choice(dart_scores)
             print(score)
             player_score += score
     total_score.append(player_score)
     player_score = 0
 
 print(f"The total score is {total_score}")
-
-#  Change name of list used to be dart_scores.
-#  Clean up code.
