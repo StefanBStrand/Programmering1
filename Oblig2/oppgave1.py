f7363e49d26fe960547f04d25d8531abe80d53be
Stefan Strand <stefanboytchev@hotmail.com>
2023-10-02 10:19:18 +0200

    Corrected task1, removed unnecessary code

--- Oblig2/oppgave1.py
+++ Oblig2/oppgave1.py
@@ -4,9 +4,10 @@ num = int(input("Hva er svaret på det ultimate spørsmålet om livet, universet
 
 if num == 42:
     print("Det stemmer, meningen med livet er 42!")
-elif 30 <= num <= 50 and num != 42:
+elif 30 <= num <= 50:   # and num != 42:
     print("Nærme, men feil.")
 else:
     print("FEIL!")
 
 # Line 7: This is a chained comparison. Simplifies alternative code like #  num >= 30 and num <=50
+# Feedback on this task from teacher: Since elif runs when if does not, this means that and num != 42 is obsolete.
