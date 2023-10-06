4eaff8369dfa868c86d55ee33ae40a0b9864e3fa
Stefan Strand <stefanboytchev@hotmail.com>
2023-10-06 22:05:29 +0200

    Checking to see why files not added via terminal. Trying commit through PyCharm instead.

---
+++ Oblig3/oppgave1.py
@@ -0,0 +1,19 @@
+#  Dictionaries
+
+student = {
+    "first_name": "Ola",
+    "last_name": "Nordmann",
+    "favourite_course": "Programmering 1"
+}
+
+#  1:
+print("First name and last name:", student["first_name"], student["last_name"])
+
+#  2:
+student["favourite_course"] = "Programmering 1, ITF10219"
+print(student["favourite_course"])
+
+#  3:
+student["age"] = 35
+print(student)
+
