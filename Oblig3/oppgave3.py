c94eed6f159fb37450afbe7fa6ce6f4bb7493371
Stefan Strand <stefanboytchev@hotmail.com>
2023-10-07 21:54:03 +0200

    Started task 4. Too tired for brain to work. To be continued

---
+++ Oblig3/oppgave3.py
@@ -0,0 +1,11 @@
+# Funksjon - printe liste
+
+
+def print_list(snacks):
+    for meal in snacks:
+        print(meal)
+
+
+favourite_meals = ["Cookies", "Protein Bars", "Chocolate Milk"]
+
+print_list(favourite_meals)
---
+++ Oblig3/oppgave4.py
@@ -0,0 +1,7 @@
+# Funksjon - Volum
+
+def calculate_volume():
+    int(input("Type in length:")),
+    int(input("Type in height:")),
+    int(input("Type in width:"))
+        
