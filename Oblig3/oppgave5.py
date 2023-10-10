cd0ed66d39b1ccd6c1c91ab97bf584ab133ee563
Stefan Strand <stefanboytchev@hotmail.com>
2023-10-10 23:26:01 +0200

    Started on task5

---
+++ Oblig3/oppgave5.py
@@ -0,0 +1,9 @@
+movies = [
+    {"name": "Inception", "Year": "2010", "Rating": "8.7"},
+    {"name": "Inside Out", "Year": "2015", "Rating": "8.1"},
+    {"name": "Con Air", "Year": "1997", "Rating": "6.9"}
+]
+
+
+def add_movie(movie_list, name, year, rating):
+    {}
