fa3547fa1bcf56f86fa04f3ab2a058c2f5170e05
Stefan Strand <stefanboytchev@hotmail.com>
2023-10-11 22:57:08 +0200

    Completed task 5.

--- Oblig3/oppgave5.py
+++ Oblig3/oppgave5.py
@@ -5,7 +5,7 @@ movies = [
 ]
 
 
-def add_movie(movie_list, name, year, rating):
+def add_movie(movie_list, name, year, rating="5.0"):
     new_movie = {"name": name,
                  "year": year,
                  "rating": rating
@@ -16,5 +16,8 @@ def add_movie(movie_list, name, year, rating):
 add_movie(movies, "Grease", "1978", "7.2")
 add_movie(movies, "Ocean's Eleven", "2001", "7.7")
 add_movie(movies, "Catch me if you can", "2002", "8.1")
+add_movie(movies, "Good Will Hunting", "1997")  # No value for rating passed in - default value applies
 
 print(movies)
+
+
