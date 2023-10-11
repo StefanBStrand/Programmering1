43af1d756ad30a30f3e320b91d26533af2e284c3
Stefan Strand <stefanboytchev@hotmail.com>
2023-10-11 21:38:25 +0200

    Completed 2/3 of task 5. Saving for last part modifications to function

--- Oblig3/oppgave5.py
+++ Oblig3/oppgave5.py
@@ -1,9 +1,20 @@
 movies = [
-    {"name": "Inception", "Year": "2010", "Rating": "8.7"},
-    {"name": "Inside Out", "Year": "2015", "Rating": "8.1"},
-    {"name": "Con Air", "Year": "1997", "Rating": "6.9"}
+    {"name": "Inception", "year": "2010", "rating": "8.7"},
+    {"name": "Inside Out", "year": "2015", "rating": "8.1"},
+    {"name": "Con Air", "year": "1997", "rating": "6.9"}
 ]
 
 
 def add_movie(movie_list, name, year, rating):
-    {}
+    new_movie = {"name": name,
+                 "year": year,
+                 "rating": rating
+                 }
+    movie_list.append(new_movie)
+
+
+add_movie(movies, "Grease", "1978", "7.2")
+add_movie(movies, "Ocean's Eleven", "2001", "7.7")
+add_movie(movies, "Catch me if you can", "2002", "8.1")
+
+print(movies)
