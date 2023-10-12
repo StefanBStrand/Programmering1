04dc0b09990d4f8a02cffdc48a3891123da5e40e
Stefan Strand <stefanboytchev@hotmail.com>
2023-10-12 22:31:09 +0200

    Completed task 5.2 B

--- Oblig3/oppgave5.2.py
+++ Oblig3/oppgave5.2.py
@@ -1,4 +1,5 @@
 # Mer funksjoner
+# 5.2 A
 
 movies = [
     {"name": "Inception", "year": "2010", "rating": "8.7"},
@@ -16,3 +17,17 @@ print_movies()
 
 # The Lion King - 1994 has a rating of 8.5 - Example printout.
 # (f"{movies[0]['name']}, {movies[0]['year']} has a rating of {movies[0]['rating']}."))
+
+# 5.2 B
+
+
+def avg_movie_rating():
+    total_rating = 0
+    for movie in movies:
+        total_rating += float(movie["rating"])
+    return round(total_rating / len(movies), 1)  # rounding the answer down to 1 decimal, for consistency reasons.
+         
+
+print(f"The average movie rating is: {avg_movie_rating()}")
+
+#  return float(movie['rating']) / len(movies)
