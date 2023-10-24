e7b56055667db3e01abe29e17507e5876c418753
Stefan Strand <stefanboytchev@hotmail.com>
2023-10-24 06:51:00 +0200

    Completed task 1A, but need to see over and refactor for part A and B

---
+++ Oblig4/oppgave1.py
@@ -0,0 +1,19 @@
+# Oppgave 1 - Klasser og objekter
+
+class Movies:
+    def __init__(self, title, year, score):
+        self.title = title
+        self.year = year
+        self.score = score
+
+    def movie_info(self):
+        return f"{self.title} - was released in {self.year} and currently has a score of {self.score}."
+
+
+movie1 = Movies("Inception", 2008, 8.8)
+movie2 = Movies("The Martian", 2015, 8.0)
+movie3 = Movies("Joker", 2019, 8.4)
+
+print(movie1.movie_info())
+print(movie2.movie_info())
+print(movie3.movie_info())
