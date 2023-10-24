a75622a215e7103e2adb119f2510c555e7e51046
Stefan Strand <stefanboytchev@hotmail.com>
2023-10-25 01:46:53 +0200

    Completed oppgave 1A and B

--- Oblig4/oppgave1.py
+++ Oblig4/oppgave1.py
@@ -15,6 +15,10 @@ movie1 = Movies("Inception", 2008, 8.8)
 movie2 = Movies("The Martian", 2015, 8.0)
 movie3 = Movies("Joker", 2019, 8.4)
 
+print(f"{movie1.title} was released in {movie1.year} and currently has a score of {movie1.score}.")
+print(f"{movie2.title} was released in {movie2.year} and currently has a score of {movie2.score}.")
+print(f"{movie3.title} was released in {movie3.year} and currently has a score of {movie3.score}.")
+
 print(movie1.movie_info())
 print(movie2.movie_info())
 print(movie3.movie_info())
