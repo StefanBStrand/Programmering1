8bba0a006c447f93c5c113fcf8a96685cae8067a
Stefan Strand <stefanboytchev@hotmail.com>
2023-11-12 20:10:28 +0100

    Working on understanding code tasks last oblig for the semester

--- Oblig5/oppgave1.2.py
+++ Oblig5/oppgave1.2.py
@@ -7,3 +7,15 @@ for x in range(5):
     numbers.append(x)
 
 print(numbers)
+
+numbers1 = []
+
+for y in range(5):
+    numbers1.insert(y, 0)
+
+# insert function used to insert an item at a specified index in a list.
+# The insert method takes 2 arguments: the first arg is the index in the list where you want to insert your new item
+# Second arg is what you want to insert into the list.
+
+print(numbers1)  # Answer is: [0, 0, 0, 0, 0]
+
