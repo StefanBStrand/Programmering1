bdf3152ebf106203abb5e277be725725f22461a8
Stefan Strand <stefanboytchev@hotmail.com>
2023-11-14 13:39:19 +0100

    task 2 complete now. Seems I have already written a lot of code nedded for task 3 in the else block of this function

--- Oblig5/Oblig5_Del2/oppgave2.py
+++ Oblig5/Oblig5_Del2/oppgave2.py
@@ -29,12 +29,38 @@ def calculate_average_ware_rating(ware):
         total_rating = sum(all_wares[ware]["ratings"])
         ratings_length = len(all_wares[ware]["ratings"])
         avg_rating = total_rating / ratings_length
-        print(f"The average rating for {ware} is {avg_rating}")
+        print(f"The average rating for {ware} is {round(avg_rating, 1)}")
+    else:
+        print("Selected ware not in stock or not available. This is the current stock:")
+        print()  # this adds a blank line for separation and readability.
+        all_wares_in_stock = all_wares.items()  # all_wares_in_stock is now a tuple with first element is the key, like
+        # amd_processor, and the second element is the value (in this case the dictionary with details of that ware.
+
+        for item, item_details in all_wares_in_stock:  # the for loop iterates and for each iteration, item variable
+            # stores the key, like amd_processor, and the item_details stores the nested dict with details
+            print(f"{item}:")  # printing out the item, e.g. amd_processor, first, like a heading.
+            for item_key, item_value in item_details.items():  # here, item_key holds the key in the details of the
+                # inner dictionary, like "name", "price", etc. item_value holds the value to the keys.
+                print(f" {item_key}: {item_value}")  # here, keys and values for inner (nested) dict is printed
+            print()  # another blank line for readability
+        
+        
 
 
-calculate_average_ware_rating("amd_processor")
+calculate_average_ware_rating("xbox")
 
 
 
 
 #  print(f"Average rating for the AMD Processor:{ws.calculate_average_ware_rating(all_wares['amd_processor'])}")
+
+"""
+Original else block: prints horisontally, need printout to be more vertical for read
+
+else:                                                                                
+    print("Selected ware not in stock or not available. This is the current stock:") 
+    all_wares_in_stock = all_wares.items()                                           
+    for item in all_wares_in_stock:                                                  
+        for ware_name in item:                                                       
+            print(ware_name)                                          
+"""
